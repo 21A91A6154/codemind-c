711e997a72aeb2a8fdf9c4590baa076efd0c8da9
21A91A6154 <97424726+21A91A6154@users.noreply.github.com>
2022-04-22 10:49:24 +0530

    #include<stdio.h> int main() {     int n,i,c=0;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=1;i<n;i++)     {         if(arr[i]%2==0)         {             if(arr[i-1]%2==1 && arr[i+1]%2==1)             {                 if(i!=n-1)                 {                     c++;                 }             }         }     }     printf("%d",c);     return 0; }

---
+++ count_even_numbers_between_odd_numbers.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,c=0;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=1;i<n;i++)
+    {
+        if(arr[i]%2==0)
+        {
+            if(arr[i-1]%2==1 && arr[i+1]%2==1)
+            {
+                if(i!=n-1)
+                {
+                    c++;
+                }
+            }
+        }
+    }
+    printf("%d",c);
+    return 0;
+}
