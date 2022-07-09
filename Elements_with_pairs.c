0f6224d8da0c01e3c2238a9ab2499982b86dac47
21A91A6154 <97424726+21A91A6154@users.noreply.github.com>
2022-07-09 11:14:31 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(n%2==0)             printf("%d ",arr[i]);         else         printf("%d ",arr[i]);     }     if(n%2==1)        printf("%d ",0);     return 0; }

--- Elements_with_pairs.c
+++ Elements_with_pairs.c
@@ -11,18 +11,11 @@ int main()
     for(i=0;i<n;i++)
     {
         if(n%2==0)
-        {
             printf("%d ",arr[i]);
-        }
         else
-        {
-            printf("%d ",arr[i]);
-        }
+        printf("%d ",arr[i]);
     }
     if(n%2==1)
-    {
-        printf("%d",0);
-    }
-    
+       printf("%d ",0);
     return 0;
 }
