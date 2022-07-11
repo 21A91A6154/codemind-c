ead6f3f8c7095176186de76812d00fd77bcd6cda
21A91A6154 <97424726+21A91A6154@users.noreply.github.com>
2022-07-11 11:35:41 +0530

    #include<stdio.h> int main() {     int n,i,sum1=0,sum2=0,k=0,j;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(i<n/2)         {             sum1=sum1+arr[i];         }         else         {             sum2=sum2+arr[i];         }     }     printf("%d ",sum1);     printf("%d",sum2);     return 0; }

--- sum_of_first_half_and_second_half_in_an_array.c
+++ sum_of_first_half_and_second_half_in_an_array.c
@@ -1,24 +1,26 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,sum=0,sum1=0;
+    int n,i,sum1=0,sum2=0,k=0,j;
     scanf("%d",&n);
     int arr[n];
     for(i=0;i<n;i++)
     {
         scanf("%d",&arr[i]);
     }
-    for(i=0;i<n/2;i++)
+    for(i=0;i<n;i++)
     {
-        sum=sum+arr[i];
+        if(i<n/2)
+        {
+            sum1=sum1+arr[i];
+        }
+        else
+        {
+            sum2=sum2+arr[i];
+        }
     }
-    for(i=n/2;i<n;i++)
-    {
-        sum1=sum1+arr[i];
-    }
-    printf("%d
-",sum);
     printf("%d
 ",sum1);
+    printf("%d",sum2);
     return 0;
 }
