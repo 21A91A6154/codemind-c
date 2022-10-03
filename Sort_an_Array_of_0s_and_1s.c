9717fd055fefed0c307dc2ad61e008849573e330
21A91A6154 <97424726+21A91A6154@users.noreply.github.com>
2022-10-03 20:04:21 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]==0)         {             printf("0 ");         }     }     for(i=0;i<n;i++)     {         if(arr[i]==1)         {             printf("1 ");         }     }     return 0; }

---
+++ Sort_an_Array_of_0s_and_1s.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]==0)
+        {
+            printf("0 ");
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]==1)
+        {
+            printf("1 ");
+        }
+    }
+    return 0;
+}
